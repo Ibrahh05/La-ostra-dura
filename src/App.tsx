be272782e831683a5d0e6594f7cc98df76ef52e2
Ibrahh05 <7464122@alu.murciaeduca.es>
2025-12-04 17:13:48 +0100

    El madafukin proyecto completo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+    return (
+        <div className="min-h-screen bg-gray-900 text-white">
+            <h1 className="text-3xl font-bold underline text-center pt-10">
+                La Ostra Dura
+            </h1>
+        </div>
+    )
+}
+
+export default App
